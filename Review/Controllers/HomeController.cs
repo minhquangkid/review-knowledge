f506969559eac4d9cc1cb1e341cba512b963b999
quang <minhquangkid@gmail.com>
2024-08-24 10:22:04 +0700

    init back-end

---
+++ Review/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Review.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("init")]
+        public IActionResult Init()
+        {
+            return Ok("hi");
+        }
+    }
+}
