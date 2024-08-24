ad403f983905a1a53cd6369f03e59911f0ce9d49
quang <minhquangkid@gmail.com>
2024-08-24 11:14:29 +0700

    add routes

--- angu_v18/src/app/app.component.ts
+++ angu_v18/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angu_v18/src/app/app.routes.ts
+++ angu_v18/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "contact" , component : ContactComponent},
+    {path: "", component: HomeComponent}
+];
